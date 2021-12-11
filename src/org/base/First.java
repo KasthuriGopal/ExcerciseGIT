91fa0911f182e0ccb56f35f2627fd30ca4d2ee0c
KathuriGopal <kasthurijoy25@gmail.com>
2021-12-11 12:29:39 +0000

    Code added

--- src/org/base/First.java
+++ src/org/base/First.java
@@ -4,5 +4,11 @@ public class First {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT");
 	}
+	void add()
+	{
+		System.out.println("Addition succuess");
+	}
+	}
+	
 
-}
+
